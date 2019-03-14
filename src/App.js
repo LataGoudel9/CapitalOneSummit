b478ea192741817d2f870dd9a9dd17dd40fc341d
Lata Goudel9 <46730351+latagoudel@users.noreply.github.com>
2019-03-14 06:41:38 -0400

    created general layout with header and album gallary structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Header from './componets/Header';
+import Footer from './componets/Footer';
+import Album from './componets/Album';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1> hi </h1>
+      <Header/>
+     <Album/>
+      <Footer/>
       </div>
     );
   }
---
+++ src/componets/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+
+      </div>
+    );
+  }
+}
+
+export default Footer;
