aacb3adf72de993c2437f9623a86f8b2ba4b3cb4
latagoudel <46730351+latagoudel@users.noreply.github.com>
2019-03-13 22:59:33 -0400

    first commit

---
+++ src/componets/Webapp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Webapp extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1> Webapp </h1>
+      </div>
+    );
+  }
+}
+
+export default Webapp;
